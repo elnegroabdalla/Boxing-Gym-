e9b765a11ab21bf069cd0f6a3ce61b12f64ef8c4
Babatunde Oshuntyi <tuyibosh@proton.me>
2025-05-19 16:54:37 -0700

    added some color as well as centering the text and images on page

---
+++ Form/form.js
@@ -0,0 +1,33 @@
+
+
+<form action="/contact" method="post">
+    <h2>Contact Us</h2>
+    <div>
+        <label for="contact-name">Name:</label>
+        <input type="text" id="contact-name" name="name" required></input>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</form>
